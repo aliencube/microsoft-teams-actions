2070a1b59a0237418b20feac056e255f4ac502b3
Justin Yoo <justin.yoojh@gmail.com>
2020-01-09 23:54:49 +1100

    Update workflow

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,12 +2,20 @@
 
 cd /app
 
+echo "$WEBHOOK_URI"
+echo "$TITLE"
+echo "$SUMMARY"
+echo "$TEXT"
+echo "$THEME_COLOR"
+echo "$SECTIONS"
+echo "$ACTIONS"
+
 dotnet restore
 dotnet build
 dotnet run --project src/GitHubActions.Teams.ConsoleApp -- \
     --webhook-uri "$WEBHOOK_URI" \
     --title "$TITLE" \
-    --summary "$SUMMARY"  \
+    --summary "$SUMMARY" \
     --text "$TEXT" \
     --theme-color "$THEME_COLOR" \
     --sections "$SECTIONS" \
