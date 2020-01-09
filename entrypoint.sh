9539fa0bde2e29e2613e78aecdb8c2d0deff7e6f
Justin Yoo <justin.yoojh@gmail.com>
2020-01-10 01:03:24 +1100

    Debug output

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,14 @@
 
 cd /app
 
+echo "URI: $WEBHOOK_URI"
+echo "Title: $TITLE"
+echo "Summary: $SUMMARY"
+echo "Text: $TEXT"
+echo "Color: $THEME_COLOR"
+echo "Sections: $SECTIONS"
+echo "Actions: $ACTIONS"
+
 dotnet restore
 dotnet build
 dotnet run --project src/GitHubActions.Teams.ConsoleApp -- \
